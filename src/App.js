0fa71708fd1f97066576cf78027cb4a642eeb342
Suraj <suraj.kawadkar14@gmail.com>
2020-12-09 14:35:40 +0530

    button added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <AddItem/>
+  <AddItem></AddItem>
       </div>
     </div>
   );
--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 
  class AddItem extends Component {
+     constructor(props) {
+         super(props)
      
+         this.state = {
+              element :''
+         }
+     }
+     handleElementChange = (event)=>{
+         this.setState({
+             element : event.target.value})
+
+     }
+     addToBucket=()=>{const {element}=this.state
+
+     }
     render() {
+        const {element}=this.state
         return (
             <div>
-                <input type="text"  />
+                <label>add task</label>
+                <input type="text" value = {this.state.element} onChange={this.handleElementChange}/>
+                <button onClick={this.handleElementChange}>Add to the bucket</button>
+       
             </div>
         )
     }
