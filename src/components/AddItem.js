bf4b455f77529c6e9caee6fde13ad1330bb80952
Suraj <suraj.kawadkar14@gmail.com>
2020-12-10 09:08:26 +0530

    styling the page but delete function doesnt run

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import '../App.css';
+import '../style.css';
 
  class AddItem extends Component {
      constructor(props) {
@@ -31,15 +31,42 @@ import '../App.css';
              
          })
      }
-     removeItem = event =>{
 
-        console.log("works")
+    //  removeItemdeleteItem(key) {
+    //     var filteredList = this.state.items.filter(function (item) {
+    //       return (item.key !== key);
+    //     });
+       
+    //     this.setState({
+    //       list: filteredItems
+    //     });
+    //   }
+/////////////////////////
+
+ removeItem = (item) =>{
+    console.log("works")
+     let filteredList = this.state.list.list.filter(list=>{
+         return(list!==item)
+     }) 
+
+     this.setState(
+       
+         this.setState({list:filteredList})
+     )
+ }
+//////////////////////////////////////////////////////
+    //  removeItem = event =>{
+
+    //     console.log("works")
+    //     console.log(index)
             
 
         
 
 
-            }
+    //         }
+
+////////////////////////////////////////////////////////
           //  list = list.splice(index,1)
           //  console.log(list)
 
@@ -54,17 +81,18 @@ import '../App.css';
         console.log(this.state.list)
         console.log("element"+element)
         
+        
         return (
             <div >
                <div className="heading">Welcome to Bucketlist</div>
 
-                <label>add task</label>
-                <input type="text" value = {this.state.element} onChange={this.handleElementChange}/>
-                <button type="submit" onClick={this.handleSubmit}>Add  the bucket</button>
+                <label>Add task</label>
+                <input type="text" value = {this.state.element} onChange={this.handleElementChange} placeholder="Enter the task"/>
+                <button type="submit" onClick={this.handleSubmit} className="add-btn">Add  the bucket</button>
        
-                <ul>
+                <ul className="ul-list">
                    {this.state.list.map((item,index)=>(
-                       <li key  = {index}>{item} <button onClick={this.removeItem} value={index}>DELETE{index}</button></li>
+                       <li key  = {index}>{item} <button onClick={(e)=>this.removeItem(item)} value={index}>DELETE{index}</button></li>
                        
                    ))}
                </ul>
